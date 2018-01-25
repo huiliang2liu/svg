39e522e9189ddc93fdd47036302c053fb6c3d34a
liuhuiiang <825378291@qq.com>
2018-01-25 16:30:37 +0800

    Create AnimEndListen.java

---
+++ src/com/xh/svg/AnimEndListen.java
@@ -0,0 +1,10 @@
+package com.xh.svg;
+
+/**
+ * @version 创建时间：2018-1-9 下午12:46:03 项目：repair 包名：com.xh.svg 文件名：AnimEnd.java
+ *          作者：lhl 说明:
+ */
+
+public interface AnimEndListen {
+	void end();
+}
